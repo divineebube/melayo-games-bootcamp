27174487f95d1355da55749b137479fcec13739f
theine <divineebube@gmail.com>
2022-09-27 12:18:20 +0100

    Update Features
    added auto spawn features for animals

--- Prototype 1/Assets/Scripts/SpawnManager.cs
+++ Prototype 1/Assets/Scripts/SpawnManager.cs
@@ -8,21 +8,26 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefab;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefab.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+      
+    }
+
+    void SpawnRandomAnimal()
+    {
+        //Spawn random animal
+        int animalIndex = Random.Range(0, animalPrefab.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             
-            Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
-        }
+        Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
     }
 }
