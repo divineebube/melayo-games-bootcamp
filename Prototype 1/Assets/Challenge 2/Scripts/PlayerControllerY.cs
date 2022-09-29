648767193d4140c0519719849b2cac51cd0bc552
theine <divineebube@gmail.com>
2022-09-29 16:17:01 +0100

    Challenge 2 Assessment Update
    I added 1 second delay time to dog spawn when the space bar is pressed

--- Prototype 1/Assets/Challenge 2/Scripts/PlayerControllerY.cs
+++ Prototype 1/Assets/Challenge 2/Scripts/PlayerControllerY.cs
@@ -5,17 +5,42 @@ using UnityEngine;
 public class PlayerControllerY : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public bool delayTime = false;
+    public float delayTimeMax;
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // On spacebar press, send dog, on a 1 second interval/delaytime
+        Reset();
+        if (Input.GetKeyDown(KeyCode.Space) && !delayTime)
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            delayTimeMax = 1.0f;
+            delayTime = true;
+           Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
             
         }
+        
     }
+    
+    private void Reset()
+    {
+        if(delayTime && delayTimeMax > 0)
+        {
+            delayTimeMax -= Time.deltaTime;
+        }
+        if(delayTimeMax < 0)
+        {
+            delayTimeMax = 0;
+            delayTime = false;
+        }
+    }
+
+    
+ 
+   
+    
+    
 
    
 }
--- Prototype 1/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Prototype 1/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,12 +11,13 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = Random.Range(3.0f, 5.0f);
+    //private float spawnInterval = Random.Range(3.0f, 5.0f);
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke("SpawnRandomBall", startDelay);
+        
     }
 
     // Spawn random ball at random x position at top of play area
@@ -31,6 +32,7 @@ public class SpawnManagerX : MonoBehaviour
         // instantiate ball at random spawn location
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
         
+        Invoke("SpawnRandomBall", Random.Range(3.0f, 5.0f));
                     
     }
 
